64822fed8f8f09f3f5d047de361b17ba79a46a0f
Andre Weissflog <floooh@gmail.com>
2023-07-08 18:25:23 +0200

    add a (hacky and hardwired-to-Mac) compile_shaders.sh script

---
+++ compile-shaders.sh
@@ -0,0 +1,6 @@
+# HACK HACK HACK, assumes Mac
+../../../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/blend/shader.glsl -o examples/blend/shader.rs -l glsl330:metal_macos:hlsl4 -f sokol_rust
+../../../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/cube/shader.glsl -o examples/cube/shader.rs -l glsl330:metal_macos:hlsl4 -f sokol_rust
+../../../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/instancing/shader.glsl -o examples/instancing/shader.rs -l glsl330:metal_macos:hlsl4 -f sokol_rust
+../../../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/mrt/shader.glsl -o examples/mrt/shader.rs -l glsl330:metal_macos:hlsl4 -f sokol_rust
+../../../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/texcube/shader.glsl -o examples/texcube/shader.rs -l glsl330:metal_macos:hlsl4 -f sokol_rust
