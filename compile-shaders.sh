6c10f3cdc2c36cea7e88ae15257e81cbfbdeb5e4
Andre Weissflog <floooh@gmail.com>
2024-04-25 19:30:16 +0200

    update compile-shaders.sh

--- compile-shaders.sh
+++ compile-shaders.sh
@@ -4,4 +4,4 @@
 ../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/instancing/shader.glsl -o examples/instancing/shader.rs -l glsl430:metal_macos:hlsl5 -f sokol_rust
 ../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/mrt/shader.glsl -o examples/mrt/shader.rs -l glsl430:metal_macos:hlsl5 -f sokol_rust
 ../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/texcube/shader.glsl -o examples/texcube/shader.rs -l glsl430:metal_macos:hlsl5 -f sokol_rust
-../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/texcube/shader.glsl -o examples/texcube/shader.rs -l glsl430:metal_macos:hlsl5 -f sokol_rust
+../sokol-tools-bin/bin/osx_arm64/sokol-shdc -i examples/vertexpull/shader.glsl -o examples/vertexpull/shader.rs -l glsl430:metal_macos:hlsl5 -f sokol_rust
