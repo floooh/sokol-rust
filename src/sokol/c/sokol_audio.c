eb316158c916ee8e24c9860cedde3f4469a0f614
GH Action <none>
2026-04-20 13:08:49 +0000

    updated (https://github.com/floooh/sokol/commit/cd33a88e0f5bf92c43d58aba6af80214380fd035)

--- src/sokol/c/sokol_audio.c
+++ src/sokol/c/sokol_audio.c
@@ -1,5 +1,5 @@
 #if defined(IMPL)
 #define SOKOL_AUDIO_IMPL
 #endif
-#include "sokol_audio.h"
 #include "sokol_defines.h"
+#include "sokol_audio.h"
--- src/sokol/c/sokol_defines.h
+++ src/sokol/c/sokol_defines.h
@@ -1,7 +1,8 @@
-#define SOKOL_RUST_BINDINGS
-#define SOKOL_NO_ENTRY
+#if !defined(__ANDROID__)
+    #define SOKOL_NO_ENTRY
+#endif
 #if defined(_WIN32)
-#define SOKOL_WIN32_FORCE_MAIN
+    #define SOKOL_WIN32_FORCE_MAIN
 #endif
 // FIXME: macOS Zig HACK without this, some C stdlib headers throw errors
 #if defined(__APPLE__)
